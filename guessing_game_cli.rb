1f2bd671586dc9ac64da9ce41ad6ffff8a0c040b
Meredith McIntosh <meredith@mcintosh.io>
2020-11-14 21:17:01 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,38 @@
-# Code your solution here!
+def run_guessing_game
+  $computerNumber = 0
+  $userGuess = ""
+  $gameStatus = "playing"
+  
+  while $gameStatus == "playing" do
+    getUserNumber()
+    getComputerNumber()
+    # Compare that input to the random number that has been generated
+    case $userGuess
+      when "exit"
+        # It the user's input is equal to "exit": Goodbye!
+        $gameStatus = "finished"
+        puts "Goodbye!"
+      when $computerNumber
+        # If the user's input matches the random number: You guessed the correct number!
+        puts "You guessed the correct number!"
+        $gameStatus = "finished"
+      else
+        # If the user's input DOES NOT matches the random number: Sorry! The computer guessed <number>.
+        puts "Sorry! The computer guessed #{$computerNumber}."
+        $gameStatus = "finished"
+    end
+  end
+
+end
+
+def getComputerNumber
+  # Generate and store a random number between 1 and 6
+  $computerNumber = (rand(1..6) + 1).to_s
+end
+
+def getUserNumber
+  # Prompts the user to guess their own number between 1 and 6
+  # Capture user input from the command line
+  puts "Guess a number between 1 and 6.\n"
+  $userGuess = gets.chomp
+end
--- spec/guessing_cli_spec.rb
+++ spec/guessing_cli_spec.rb
@@ -1,28 +1,25 @@
 require_relative 'spec_helper'
 
 describe "Guessing CLI" do
-
   context 'user inputs' do
+    
     it "responds to 'exit'" do
       expect(self).to receive(:gets).and_return('exit')
       expect { run_guessing_game }.to output(/Goodbye!/).to_stdout
     end
-
-    it "responds to an incorrect guess" do
-      allow(self).to receive(:rand).and_return(5)
-
-      expect(self).to receive(:gets).and_return("2")
-
-      expect { run_guessing_game }.to output(/Sorry! The computer guessed 6./).to_stdout
-    end
-
+    
     it "responds to a correct guess" do
       allow(self).to receive(:rand).and_return(1)
-
-      expect(self).to receive(:gets).and_return("2")
-
+      expect(self).to receive(:gets).and_return("1")
       expect { run_guessing_game }.to output(/You guessed the correct number!/).to_stdout
     end
+    
+    it "responds to an incorrect guess" do
+      allow(self).to receive(:rand).and_return(5)
+      expect(self).to receive(:gets).and_return("2")
+      expect { run_guessing_game }.to output(/Sorry! The computer guessed 2./).to_stdout
+    end
+
   end
 
 
